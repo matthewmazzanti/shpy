25978a33f4d7a08495a739eb0756d5708f80cb85
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2023-09-23 16:31:12 -0400

    Add output samples

--- test.py
+++ test.py
@@ -2,12 +2,36 @@ from sh import builder as b, run, run_json, ExecutableError, show
 
 print("Simple command, capture output to a string:")
 print(b.ls)
+"""
+LICENSE
+README.md
+__pycache__
+sh.py
+test.py
+wip.py
+"""
 
 print("Command with flags")
 print(b.ls("-la"))
+"""
+total 24
+drwxr-xr-x 10 user user  320 Sep 23 16:19 .
+drwxr-xr-x 19 user user  608 Sep 23 15:28 ..
+drwxr-xr-x 13 user user  416 Sep 23 16:19 .git
+-rw-r--r--  1 user user   22 Sep 23 16:15 .gitignore
+-rw-r--r--  1 user user 1060 Sep 23 16:19 LICENSE
+-rw-r--r--  1 user user   83 Sep 23 16:18 README.md
+drwxr-xr-x  3 user user   96 Sep 23 16:13 __pycache__
+-rw-r--r--  1 user user 1832 Sep 23 16:13 sh.py
+-rw-r--r--  1 user user  921 Sep 23 16:15 test.py
+-rw-r--r--  1 user user  485 Sep 23 15:31 wip.py
+"""
 
 print("Simple pipeline:")
 print(b.ls("-la") | b.grep("sh"))
+"""
+-rw-r--r--  1 mmazzanti staff 1832 Sep 23 16:13 sh.py
+"""
 
 try:
     data = run_json(b.docker.image.ls("--all", "--format=json") | b.jq("-s"))
